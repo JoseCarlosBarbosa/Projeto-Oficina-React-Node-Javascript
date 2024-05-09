5ad4ae9085f0edb310433b04fe125bc7b15b9b69
José Carlos Barbosa <carlinho.skillet@gmail.com>
2024-05-09 19:21:29 -0300

    CadastroCliente JS ligação com backend e modificações no server para cadastroC

--- client/src/componets/componetsAgrupados/CadastroCliente.js
+++ client/src/componets/componetsAgrupados/CadastroCliente.js
@@ -1,48 +1,102 @@
-
-import React from "react";
-import { Link } from "react-router-dom";
-
-
-
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
 
 function CcadastroCliente() {
+  const [name, setName] = useState('');
+  const [cpf, setCpf] = useState('');
+  const [tel, setTel] = useState('');
+  const [endereco, setEndereco] = useState('');
 
-    return (
+  const config = {
+    Headers: {
+      'Content-Type': 'application/json',
+    },
+  };
 
+  const handleCadastro = async e => {
+    e.preventDefault();
 
-        <div className="main">
-            <h2 className="titulo-pag">Cadastro Cliente</h2>
+    try {
+      const response = await axios.post(
+        'http://localhost:3001/cadastro-cliente',
+        {
+          name: name,
+          cpf: cpf,
+          tel: tel,
+          endereco: endereco,
+        },
+      );
+      console.log('resposta do servidor: ', response.data);
 
-            <div className="central">
-                <div className="central-2">
+      setName('');
+      setCpf('');
+      setEndereco('');
+      setTel('');
+    } catch (error) {
+      console.error('erro ao cadastrar cliente', error);
+    }
+  };
 
-                    <input className="form-control" type="text" placeholder="Nome" />
-                    <div>
-                        <input className="form-control" type="text" placeholder="CPF" />
-                    </div>
-                    <div>
-                        <input className="form-control" type="text" placeholder="Telefone" />
-                    </div>
-                    <div>
-                        <input className="form-control" type="text" placeholder="Endereço" />
-                    </div>
-                    <div>
-                        <input className="form-control" type="text" placeholder="ID" disabled />
-                    </div>
-                    <Link to="/">
+  return (
+    <main>
+      <div className="main">
+        <h2 className="titulo-pag">Cadastro Cliente</h2>
 
-                        <button className="botaoSalvar" type="button" >Salvar</button>
-
-                    </Link>
-                </div>
+        <div className="central">
+          <div className="central-2">
+            <input
+              className="form-control"
+              type="text"
+              id="name-client-cadastro"
+              placeholder="Nome"
+              value={name}
+              onChange={e => setName(e.target.value)}
+            />
+            <div>
+              <input
+                className="form-control"
+                type="text"
+                id="CPF-client-cadastro"
+                placeholder="CPF"
+                value={cpf}
+                onChange={e => setCpf(e.target.value)}
+              />
             </div>
+            <div>
+              <input
+                className="form-control"
+                type="text"
+                id="tel-client-cadastro"
+                placeholder="Telefone"
+                value={tel}
+                onChange={e => setTel(e.target.value)}
+              />
+            </div>
+            <div>
+              <input
+                className="form-control"
+                type="text"
+                id="end-client-cadastro"
+                placeholder="Endereço"
+                value={endereco}
+                onChange={e => setEndereco(e.target.value)}
+              />
+            </div>
+            <Link to="/">
+              <button
+                onClick={e => handleCadastro(e)}
+                className="botaoSalvar"
+                type="submit"
+                id="btn-client-submit">
+                Salvar
+              </button>
+            </Link>
+          </div>
         </div>
-
-
-    );
-
+      </div>
+    </main>
+  );
 }
 
 export default CcadastroCliente;
-
-
--- server.js
+++ server.js
@@ -2,11 +2,22 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.send('Servidor Node.js funcionando!');
+  res.send('Servidor Node.js funcionando!');
 });
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+app.post('/cadastro-cliente', (req, res) => {
+  const {name, cpf, tel, endereco} = req.body;
+  console.log('Dados do Formulario: ', {name, cpf, tel, endereco});
+  res.send('dados enviados');
 });
